3dd0ff5a5ac9841e1382efae61afd710f7fd10ac
WayupKG <adikgk232323@gmail.com>
2025-12-27 19:52:46 +0600

    Add new keyboard generation methods: `get_on_btn_register_kb`, `get_phone_request_kb`, and `get_on_btn_generate_code_kb`, and enhance `get_on_start_kb` formatting

--- src/keyboards/common_keyboards.py
+++ src/keyboards/common_keyboards.py
@@ -1,7 +1,12 @@
 from aiogram.types import (
     KeyboardButton,
     ReplyKeyboardMarkup,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
 )
+
+from enums.commons import ClientCodeTypeEnum
+
 
 class ButtonText:
     MY_CODE = "🎫 Мой код"
@@ -15,16 +20,65 @@ class ButtonText:
 
 def get_on_start_kb() -> ReplyKeyboardMarkup:
     buttons = [
-        [KeyboardButton(text=ButtonText.MY_CODE),
-         KeyboardButton(text=ButtonText.PACKAGES)],
-        [KeyboardButton(text=ButtonText.CHINA_ADDRESS),
-         KeyboardButton(text=ButtonText.PVZ_ADDRESS)],
-        [KeyboardButton(text=ButtonText.TARIFFS),
-         KeyboardButton(text=ButtonText.SUPPORT)],
+        [
+            KeyboardButton(text=ButtonText.MY_CODE),
+            KeyboardButton(text=ButtonText.PACKAGES),
+        ],
+        [
+            KeyboardButton(text=ButtonText.CHINA_ADDRESS),
+            KeyboardButton(text=ButtonText.PVZ_ADDRESS),
+        ],
+        [
+            KeyboardButton(text=ButtonText.TARIFFS),
+            KeyboardButton(text=ButtonText.SUPPORT),
+        ],
         [KeyboardButton(text=ButtonText.LANGUAGE)],
     ]
 
+    return ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
+
+
+def get_on_btn_register_kb() -> InlineKeyboardMarkup:
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="Зарегистрироватся",
+                    callback_data="register_client",
+                ),
+            ]
+        ]
+    )
+
+
+def get_phone_request_kb() -> ReplyKeyboardMarkup:
     return ReplyKeyboardMarkup(
-        keyboard=buttons,
-        resize_keyboard=True
+        keyboard=[
+            [
+                KeyboardButton(
+                    text="📞 Поделиться номером телефона",
+                    request_contact=True,
+                    callback_data="request_phone",
+                )
+            ]
+        ],
+        resize_keyboard=True,
+        one_time_keyboard=True,
+    )
+
+
+def get_on_btn_generate_code_kb() -> InlineKeyboardMarkup:
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="🈲 Китайский код",
+                    callback_data=ClientCodeTypeEnum.CHINA_NICKNAME,
+                ),
+                InlineKeyboardButton(
+                    text="🆔 Обычный код",
+                    callback_data=ClientCodeTypeEnum.DIGITAL,
+                ),
+            ]
+        ]
     )
